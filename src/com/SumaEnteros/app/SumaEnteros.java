5a61db82cd9549e1c35a688de5a87344f58683e6
Mabestron <ivangamaliel555@hotmail.com>
2019-11-15 23:53:42 -0600

    AscDesc

---
+++ src/com/SumaEnteros/app/SumaEnteros.java
@@ -0,0 +1,17 @@
+package com.SumaEnteros.app;
+
+public class SumaEnteros 
+{
+	public static void main(String[]args)
+	{
+		int aux=0;
+		
+		for(int i=0;i<=100;i++)
+		{
+			aux=aux+i;		
+		}
+		
+		System.out.println(aux);
+	}
+	
+}
